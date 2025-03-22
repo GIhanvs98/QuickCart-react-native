2612c89e255444fa019cf024d23356cd15b51d2a
Gihanvs98 <darkcreationsl98@gmail.com>
2025-03-23 01:08:53 +0530

    tab navigator

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Image, StyleSheet, View, Text, Platform } from "react-native";
 import SplashScreen from "@/components/ui/screen/SplashScreen";
 import Dashboard from "@/components/ui/screen/Dashboard";
 
-// Replace with your actual main screen
+
 
 export default function HomeScreen() {
     const [isLoading, setIsLoading]=useState(true);
---
+++ app/navigation/tab-navigation/HomeBottomTabNavigation.tsx
@@ -0,0 +1,35 @@
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import HomeBookMarkScreen from "@/components/ui/screen/home/HomeBookMarkScreen";
+import HomeMenuScreen from "@/components/ui/screen/home/HomeMenuScreen";
+import HomeCartScreen from "@/components/ui/screen/home/HomeCartScreen";
+import HomePageScreen from "@/components/ui/screen/home/HomePageScreen";
+import {COLORS} from "@/constants/ColorPallet";
+const Tab =createBottomTabNavigator();
+export default function HomeBottomTabNavigation({navigation}:any){
+    return(
+        <Tab.Navigator
+        initialRouteName={'Home'}
+        screenOptions={({route, focused}:any)=>({
+            tabBarIcon:({color, size})=>{
+                let iconName;
+                if(route.name==='Menu') iconName = focused? 'menu':'menu-outline';
+                else if(route.name==='Cart') iconName = focused? 'cart':'cart-outline';
+                else if(route.name==='Home') iconName = focused? 'home':'home-outline';
+                else if(route.name==='Bookmarks') iconName = focused? 'heart':'heart-outline';
+                else if(route.name==='Products') iconName = focused? 'grid':'grid-outline';
+                // @ts-ignore
+                return <Ionicons name={iconName} size={22} color={color}/>
+            },
+            tabBarActiveTintColor:COLORS.purple,
+            tabBarInactiveTintColor:COLORS.darkGray
+        })}
+        >
+            <Tab.Screen name={'Menu'} component={HomeMenuScreen}/>
+            <Tab.Screen name={'Cart'} component={HomeCartScreen}/>
+            <Tab.Screen name={'Home'} component={HomePageScreen}/>
+            <Tab.Screen name={'Bookmarks'} component={HomeBookMarkScreen}/>
+            <Tab.Screen name={'Products'} component={HomeBookMarkScreen}/>
+
+        </Tab.Navigator>
+    )
+}
